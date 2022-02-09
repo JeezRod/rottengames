3885932078c535a95c69ca16b858cddc2bc5e6eb
Sirine Aoudj <sirine.aoudj@dawsoncollege.qc.ca>
2022-02-09 14:16:56 -0500

    added user login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,44 @@
 import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Games from "./components/Games";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import GoogleLogin, { GoogleLogout } from 'react-google-login';
 
 function App() {
 
   const [data, setData] = React.useState(null);
+  const [username, setUserName] = React.useState("");
+  const handleLogin = async googleData => {
+    const res = await fetch("/api/v1/auth/google", {
+      method: "POST",
+      body: JSON.stringify({
+        token: googleData.tokenId
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const data = await res.json()
+    console.log("email:"+data.email)
+    setUserName(data.email)
+  }
+
+  const handleLogout = async response => {
   
+    const res = await fetch("/api/v1/auth/logout", {
+     method: "DELETE",
+ 
+ })
+ const data = await res.json()
+ setUserName("");
+}
+
+
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
@@ -21,17 +48,33 @@ function App() {
   return (
     <Router>
       <div className="NavContainer">
-          <Navbar ></Navbar>
+        <Navbar ></Navbar>
+        <br></br>
+        <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText="Log in with Google"
+          onSuccess={handleLogin}
+          onFailure={handleLogin}
+          cookiePolicy={'single_host_origin'}
+        />
+        <GoogleLogout
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText="Logout"
+          onLogoutSuccess={handleLogout}
+        />
+
+        <p>Hello {username === "" ? "Anonymous" : username}</p>
+
       </div>
       <Routes>
         <Route path="/" element={<App />} />
         <Route index element={<Home />} />
-        <Route path="about" element={<About/>}/>
-        <Route path="games" element={<Games/>}/>
-        <Route path="login" element={<Login />}/>
-        <Route path="register" element={<Register />}/>
+        <Route path="about" element={<About />} />
+        <Route path="games" element={<Games />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
-    </Router>  
+    </Router>
   );
 }
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const session = require("express-session");
 
 router.use(express.json());
 
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+
+const users = new Array();
+
 //Sample get route (/api/)
 router.get("/", (req, res) => {
-    res.json({message: "Work in progress!"})
+    res.json({ message: "Work in progress!" })
 });
+
+router.post("/v1/auth/google", async (req, res) => {
+    const { token } = req.body    
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.REACT_APP_GOOGLE_CLIENT_ID
+    });
+    const { name, email, picture } = ticket.getPayload(); 
+    const user = {"name": name,"email": email,"picture": picture};
+    const existsAlready = users.findIndex(element => element.email === email)
+    users.push(user);
+    req.session.userId = email
+    console.log(req.session.userId);  
+    res.status(201)
+    res.json(user)
+})
 
 // Here you can find an incomplete list of routes that we can use to access the database.
 // The routes simply return a json with a message for now, we need to make database functions
@@ -17,25 +39,25 @@ router.get("/", (req, res) => {
 
 //Route to get all games in the database (/api/games)
 router.get("/games", (req, res) => {
-    res.json({message: "Get all the games"})
+    res.json({ message: "Get all the games" })
 });
 
 //Route to get a specific game in the database (/api/games/:id)
 router.get("/games/:gameId", (req, res) => {
     //we can use req.params.gameId to send the id to the db to find its information
-    res.json({message: "Getting information for game with id: "+req.params.gameId})
+    res.json({ message: "Getting information for game with id: " + req.params.gameId })
 });
 
 //Route to get all reviews for a specific game in the database (/api/games:id/reviews)
 router.get("/games/:id/reviews", (req, res) => {
     //we can use req.params.id to send the id to the db to get all reviews related to it
-    res.json({message: "Getting all reviews for game with id: "+req.params.id})
+    res.json({ message: "Getting all reviews for game with id: " + req.params.id })
 });
 
 //Route to get all reviews for a specific game in the database (/api/games:id/reviews/:id)
 router.get("/games/:gameId/reviews/:reviewId", (req, res) => {
     //we can use req.params.gameId to send the id to the db to get the review with the id req.params.reviewId
-    res.json({message: "Getting review with id: "+ req.params.reviewId+" for game with id: "+req.params.gameId})
+    res.json({ message: "Getting review with id: " + req.params.reviewId + " for game with id: " + req.params.gameId })
 });
 
 //POST Routes
