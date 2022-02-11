32e453c785f7a4d8b2c4ef5f5e9f59fdd8d9d65e
Le Duytam Ly <le.ly@dawsoncollege.qc.ca>
2022-02-10 23:21:29 -0500

    Add fetch game from server in react

--- client/src/components/GridView/index.js
+++ client/src/components/GridView/index.js
@@ -1,14 +1,43 @@
 import React from 'react'
 import GameCard from '../GameCard'
-import data from '../sample.json'
 import './GridView.css'
 
-const GridView = () => {
+function GridView(){
+  //State for the games
+  const [data, setData] = React.useState([]);
+  //State for the loading state
+  const [loading, setLoading] = React.useState(false)
+  
+  React.useEffect( () => {
+    //Async function to fetch all the games
+    async function fetchData() {
+      try{
+
+      setLoading(true)
+      let data = await fetch("/api/games");
+      let dataJson = await data.json();
+      await setData(dataJson);
+
+      } catch(e){
+        console.log("Fetch error")
+      } finally{
+        setLoading(false)
+      }
+    }
+    fetchData();
+  },[]);
+  
+  //If the games are loading show loading prompt
+  if(loading){
+    return <p>Loading...</p>
+  }
+
+  //Else render the gridview of all the games
   return (
     <div className='gridview'>
-        {data.map(game => {
+      {data.map(game => {
             return <GameCard key={game._id.$oid} game={game}/>
-        })}
+      })}
     </div>
   )
 }
