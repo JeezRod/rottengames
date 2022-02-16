7d007c185f2c612e75284e87ea7a028b095f32a9
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2022-02-16 16:12:48 -0500

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const app = require("./app");
-
 const PORT = process.env.PORT || 3001;
+const conn = require("./db/conn");
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
-  
+(async ()=> {
+    await conn().catch(err => console.log(err));
+    app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`);
+    });
+})();
