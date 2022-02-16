30874487a834313570806d39337a5ce14b3000e0
Jei wen Wu <jei_wu@hotmail.com>
2022-02-16 15:45:15 -0500

    Rating stars added

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Review.css"
 import HaloInf from "./Images/HaloInf.png";
 import ReviewCard from "../ReviewCard";
+import StarRating from "../StarRating";
 
 function Review(game) {
   return (
@@ -10,18 +11,24 @@ function Review(game) {
             <img className="GameCover" src={HaloInf} alt="HaloInfinite"></img>
             <div className="NameStars">
               <h1>Halo Infinite</h1>
-              <p>rating stars api</p>
+              <StarRating />
               <button className="AdminButton">Edit page</button>
             </div>
           </div>
 
         <div className="Description">
-          <p>Description</p>
+          <h1>Description</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          
         </div>
         <div className="Review">
             <h1>Reviews</h1>
 
             <ReviewCard review={{text: "Game is cool!"}}/>
+            <ReviewCard review={{text: "Game is awesome!"}}/>
+            <ReviewCard review={{text: "Game is insane!"}}/>
+            <ReviewCard review={{text: "Game is trash!"}}/>
+            
 
         </div>
 
