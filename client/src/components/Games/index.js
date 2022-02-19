5927ce43d735a7cadc43b47e6a6df302e2bdb417
Le Duytam Ly <le.ly@dawsoncollege.qc.ca>
2022-02-18 19:20:33 -0500

    Add pagination with fix number of pages

--- client/src/components/Games/index.js
+++ client/src/components/Games/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import GridView from "../GridView";
+import ReactPaginate from 'react-paginate';
 
 function Games() {
+  //Page number
+  const [page, setPage] = React.useState(1);
+
+  const handlePageClick = (event) => {
+    console.log(event.selected)
+    setPage(event.selected+1)
+  };
+
   return (
       <div className="Games">
-          <GridView></GridView>
+        <GridView page = {page}></GridView>
+
+        <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={0}
+        marginPagesDisplayed={5}
+        pageCount={25}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
       </div>
       
   );
