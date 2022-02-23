da74f91d429ea382b93a465d7b1d1cef455ca168
Jei wen Wu <jei_wu@hotmail.com>
2022-02-23 12:49:34 -0500

    Added the reviews for a game from the database

--- client/src/components/ReviewCard/index.js
+++ client/src/components/ReviewCard/index.js
@@ -7,7 +7,7 @@ const ReviewCard = ({ review }) => {
     <div className="Rating">
       <div className="CommentCard">
         <p>{review.text}</p>
-        <StarRating review={{ stars: 0 }}/>
+        <StarRating review={review}/>
       </div>
       <button className="UserButton">Comment</button>
       <button className="AdminButton">Delete</button>
