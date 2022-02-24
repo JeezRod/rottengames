0506d5f6eeff7ea9653e2e837256f74ddd410fbd
Sirine Aoudj <sirineaoudj2@gmail.com>
2022-02-24 00:46:53 -0500

    put the login and logout button in the nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -9,61 +9,12 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import GoogleLogin, { GoogleLogout } from 'react-google-login';
 import GamePage from "./components/GamePage";
-import { useParams } from "react-router-dom";
 
 
 function App() {
-
-  const [data, setData] = React.useState([]);
-  const [username, setUserName] = React.useState("");
   
-  const handleLogin = async googleData => {
-    const res = await fetch("/api/v1/auth/google", {
-      method: "POST",
-      body: JSON.stringify({
-        token: googleData.tokenId
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    let data = await res.json()
-    console.log("email:"+ data.email)
-    setUserName(data.email)
-  }
+  const [data, setData] = React.useState(null);
   
-  useEffect(() => {
-    async function fetchMyAPI() {
-      const res = await fetch("api/user")
-      console.log("res"+res)
-      let data = await res.json()
-      console.log("email:"+ data)
-      //setUserName(data.email)
-    }
-    fetchMyAPI()
-  }, [])
-
-  const handleLogout = async response => {
-  
-    const res = await fetch("/api/v1/auth/logout", {
-     method: "DELETE",
- 
-  })
-  const data = await res.json()
-  setUserName("");
-  }
-
-  async function fetchData() {
-    //Fetching the data
-    let data = await fetch("/api/users");
-    let dataJson = await data.json();
-    //Set the returned data
-    //console.log(dataJson);
-    //await setTotalUsers(dataJson);
-  }
-  fetchData();
-
-
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
@@ -75,21 +26,6 @@ function App() {
       <div className="NavContainer">
         <Navbar ></Navbar>
         <br></br>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-          buttonText="Log in with Google"
-          onSuccess={handleLogin}
-          onFailure={handleLogin}
-          cookiePolicy={'single_host_origin'}
-        />
-        <GoogleLogout
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-          buttonText="Logout"
-          onLogoutSuccess={handleLogout}
-        />
-
-        <p>Hello {username === "" ? "Anonymous" : username}</p>
-
       </div>
       <Routes>
         <Route path="/" element={<App />} />
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,6 +3,7 @@ import './nav.css'
 import Hamburger from 'hamburger-react'
 import { NavLink } from "react-router-dom";
 import styled from 'styled-components'
+import GoogleLogin, { GoogleLogout } from 'react-google-login';
 
 //Style fo NavLink
 const StyledNav = styled(NavLink)`
@@ -15,6 +16,55 @@ const StyledNav = styled(NavLink)`
 `;
 
 const Navbar = () => {
+
+  const [data, setData] = React.useState([]);
+  const [username, setUserName] = React.useState("");
+  
+  const handleLogin = async googleData => {
+    const res = await fetch("/api/v1/auth/google", {
+      method: "POST",
+      body: JSON.stringify({
+        token: googleData.tokenId
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    let data = await res.json()
+    console.log("email:"+ data.email)
+    setUserName(data.email)
+  }
+  
+  React.useEffect(() => {
+    async function fetchMyAPI() {
+      const res = await fetch("api/user")
+      let data = await res.json()
+      console.log("email:"+ data)
+      setUserName(data)
+    }
+    fetchMyAPI()
+  }, [])
+
+  const handleLogout = async response => {
+  
+    const res = await fetch("/api/v1/auth/logout", {
+     method: "DELETE",
+ 
+  })
+  const data = await res.json()
+  setUserName("");
+  }
+
+  async function fetchData() {
+    //Fetching the data
+    let data = await fetch("/api/users");
+    let dataJson = await data.json();
+    //Set the returned data
+    //console.log(dataJson);
+    //await setTotalUsers(dataJson);
+  }
+  fetchData();
+
   return (
   <header>
     <Hamburger direction="right" />
@@ -25,8 +75,21 @@ const Navbar = () => {
             <StyledNav to="about">About </StyledNav>
         </ul>
     </nav>
+    <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText="Log in with Google"
+          onSuccess={handleLogin}
+          onFailure={handleLogin}
+          cookiePolicy={'single_host_origin'}
+    />
 
-    <StyledNav to="/login">Login</StyledNav>
+    <GoogleLogout
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText="Logout"
+          onLogoutSuccess={handleLogout}
+      />
+
+    <p>Hello {username === "" ? "Anonymous" : username}</p>
   </header>);
 };
 
