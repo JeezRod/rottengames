a577ebcc7d1fd54a9c37e01801fc0a34e84678ce
Le Duytam Ly <le.ly@dawsoncollege.qc.ca>
2022-03-07 12:43:14 -0500

    move navbar to hamburger component

---
+++ client/src/components/HamburgerMenu/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
+import { slide as Menu } from 'react-burger-menu'
+import "./HamburgerMenu.css";
+import styled from 'styled-components'
+
+
+
+//Style fo NavLink
+const StyledNav = styled(NavLink)`
+  display: inline-block;
+  padding-left: 100px;
+  font-weight: 700;
+  font-size: 18px;
+  text-decoration: none;
+  color:black;
+`;
+
+
+const HamburgerMenu = () =>{
+    return(
+        <Menu>
+            <StyledNav to="/"> Home </StyledNav>
+            <StyledNav to="/games"> Search </StyledNav>
+            <StyledNav to="about">About </StyledNav>
+            <StyledNav to="dashboard">Dashboard </StyledNav>
+        </Menu>
+    )
+}
+
+export default HamburgerMenu;
