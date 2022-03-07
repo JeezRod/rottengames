2347613713a4fa65b712137baa4e66fedc81e038
Le Duytam Ly <le.ly@dawsoncollege.qc.ca>
2022-03-07 10:46:39 -0500

    Add fetch user for profile page

--- client/src/components/UserProfile/index.js
+++ client/src/components/UserProfile/index.js
@@ -2,12 +2,33 @@ import React from 'react'
 import "./userProfile.css"
 
 const UserProfile = () => {
-  
-  return (
-    <div className='profile'>
-        <h1>Profile</h1>
-    </div>
-  )
+    //State for page number
+    const [user, setUser] = React.useState({});
+
+    //Loading the state from local storoge when the page loads
+    React.useEffect(() => {
+        async function fetchUser(){
+            let response = await fetch('/api/user');
+            if (response.status === 200) {
+            let userJson = await response.json();
+                console.log(userJson);
+                setUser(userJson);
+            } else {
+                console.log("no usere");
+            }
+        }
+        fetchUser();
+    },[]);
+
+    return (
+        <div className='profile'>
+            <h1>Profile</h1>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user._id}</p>
+            <p>{user.picture}</p>
+        </div>
+    )
 }
 
 export default UserProfile
