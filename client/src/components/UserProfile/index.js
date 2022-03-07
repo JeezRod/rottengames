27e5c73215dff3dbc44a8e2931b5717a4d8feca4
Le Duytam Ly <le.ly@dawsoncollege.qc.ca>
2022-03-07 18:14:13 -0500

    Start style for profile page

--- client/src/components/UserProfile/index.js
+++ client/src/components/UserProfile/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import "./userProfile.css"
 import { useParams } from "react-router-dom";
+import ReactLoading from "react-loading";
 
 const UserProfile = () => {
     const params = useParams();
-    //State for page number
+    //State for the user
     const [user, setUser] = React.useState({});
+    //State for loading
+    const [loading, setLoading] = React.useState(true)
 
-    //Loading the state from local storoge when the page loads
+    //Loading the user from the url params
     React.useEffect(() => {
         async function fetchUser(){
-            console.log(params.id)
+            setLoading(true)
             let response = await fetch('/api/user/'+params.id);
             if (response.status === 200) {
             let userJson = await response.json();
@@ -21,15 +24,38 @@ const UserProfile = () => {
             }
         }
         fetchUser();
+        setLoading(false)
     },[]);
+
+    //If the profile is loading show loading prompt
+    if (loading) {
+        return (
+        <div className='loading'>
+            <ReactLoading type={"spin"} color="#000" />
+        </div>
+        )
+    }
 
     return (
         <div className='profile'>
-            <h1>Profile</h1>
-            <p>{user.name}</p>
-            <p>{user.email}</p>
-            <p>{user._id}</p>
-            <p>{user.picture}</p>
+            
+            <aside className='user'>
+                <h2>Your Profile</h2>
+                <img className='profilePicture' src={user.picture} alt="profile"></img>
+                
+                <div className='userSection'>
+                    <h2>{user.name}</h2>
+                    <p>{user.email}</p>
+                </div>
+                <div className='userSection'>
+                    <h2>Bio</h2>
+                    <p>I am pro gamer</p>
+                </div>
+            </aside>
+
+            <main className='mainProfile'>
+                <h2>All Reviews</h2>
+            </main>
         </div>
     )
 }
