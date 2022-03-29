2eb0e73a67174cbdc1eb71f9598398bc1885eddb
Le Duytam Ly <le.ly@dawsoncollege.qc.ca>
2022-03-29 12:18:55 -0400

    Add background image and team section

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "../../assets/xenia-radchenko-ezEn4jYrVYQ-unsplash.jpg"
+//import image from "./xenia-radchenko-ezEn4jYrVYQ-unsplash.jpg"
 
 export default function About() {
 
@@ -16,15 +16,32 @@ export default function About() {
         </div>
 
         <div className="w-1/2">
-          <div className="">
-            <img src={image} alt="arcade" className="object-cover h-screen w-full"></img>
-            
+          <div className="h-screen flex items-end bg-cover bg-[url('/src/assets/lorenzo-herrera-p0j-mE6mGo4-unsplash.jpg')]">
+            <p className="text-3xl text-white font-bold p-10">Datasets from Kaggle,<br></br> Game covers scrapped from Google </p>
           </div>
         </div>
       </div>
 
-      <div className="h-screen " >
-        <p>Our Team.</p>
+      <div className="h-screen flex flex-col" >
+        <div className="h-1/2 flex items-center justify-center">
+          <p className="text-7xl font-bold h-1/4">Our Team.</p>
+        </div>
+
+        <div className="h-1/2 flex ">
+          <div className="w-1/2 p-10">
+            <p className="text-4xl ">Banatech is a dedicated software development company which strives for quality and customer satisfaction.</p>
+          </div>
+
+          <div className="w-1/2 p-10">
+            <p className="text-2xl font-bold">Developers</p>
+            <p className="text-2xl  mt-10">Sirine</p>
+            <p className="text-2xl  mt-5">Asli</p>
+            <p className="text-2xl  mt-5">Jei</p>
+            <p className="text-2xl  mt-5">Rodrigo</p>
+            <p className="text-2xl  mt-5">Le</p>
+          </div>
+
+        </div>
       </div>
       
       <p></p>
@@ -32,4 +49,5 @@ export default function About() {
     </div>
   );
 }
+// <img src={image} alt="arcade" className="object-cover h-screen w-full"></img>
 //<p className="text-4xl p-10">Datasets from Kaggle, Game covers scrapped from Google </p>
