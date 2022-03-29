092de76f8a324b005c60f782b45fa68f2d21724c
Le Duytam Ly <le.ly@dawsoncollege.qc.ca>
2022-03-29 19:19:45 -0400

    Add Darkmode support to about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -5,11 +5,11 @@ export default function About() {
 
   return (
     <div className="About dark:text-white">
-      <div className="h-screen flex justify-center items-center"> 
+      <div className="h-screen flex flex-col justify-center items-center"> 
         <p className="font-bold text-9xl">About.</p>
       </div>
 
-      <div className="h-screen flex bg-gray-200 dark:bg-white dark:text-white" >
+      <div className="h-screen flex bg-gray-200 dark:bg-gray-800" >
         <div className="flex flex-col justify-center items-center w-1/2" >
           <p className="font-bold text-8xl p-10">All Game reviews in one place.</p>
           
@@ -29,16 +29,16 @@ export default function About() {
 
         <div className="h-1/2 flex ">
           <div className="w-1/2 p-10">
-            <p className="text-4xl ">Banatech is a dedicated software development company which strives for quality and customer satisfaction.</p>
+          <p className="text-4xl "><strong>Banatech</strong> is a dedicated software development company which strives for quality and customer satisfaction.</p>
           </div>
 
           <div className="w-1/2 p-10">
             <p className="text-2xl font-bold">Developers</p>
-            <p className="text-2xl  mt-10">Sirine</p>
-            <p className="text-2xl  mt-5">Asli</p>
-            <p className="text-2xl  mt-5">Jei</p>
-            <p className="text-2xl  mt-5">Rodrigo</p>
-            <p className="text-2xl  mt-5">Le</p>
+            <p className="text-2xl  mt-10">Sirine Aoudj</p>
+            <p className="text-2xl  mt-5">Le Duytam Ly</p>
+            <p className="text-2xl  mt-5">Rodrigo Rivas Alfaro</p>
+            <p className="text-2xl  mt-5">Jei Wen Wu</p>
+            <p className="text-2xl  mt-5">Asli Zeybek</p>
           </div>
 
         </div>
