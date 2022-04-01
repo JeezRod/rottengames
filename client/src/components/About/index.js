e2ba78db8b3685052066520d59aa947c586c635b
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2022-04-01 13:53:13 -0400

    add responsive to about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,37 +6,39 @@ export default function About() {
   return (
     <div className="About dark:text-white">
       <div className="h-screen flex flex-col justify-center items-center"> 
-        <p className="font-bold text-9xl">About.</p>
+        <p className="font-bold text-7xl md:text-8xl lg:text-9xl">About.</p>
       </div>
 
-      <div className="h-screen flex bg-gray-200 dark:bg-gray-800" >
-        <div className="flex flex-col justify-center items-center w-1/2" >
-          <p className="font-bold text-8xl p-10">All Game reviews in one place.</p>
+      <div className="h-screen flex flex-col lg:flex-row bg-gray-200 dark:bg-gray-800" >
+
+        <div className="flex flex-col justify-center items-center w-screen h-1/2 lg:h-screen lg:w-1/2" >
+          <p className="font-bold text-6xl md:text-7xl lg:text-8xl p-10">All Game reviews in one place.</p>
           
         </div>
 
-        <div className="w-1/2">
-          <div className="h-screen flex items-end bg-cover bg-[url('/src/assets/lorenzo-herrera-p0j-mE6mGo4-unsplash.jpg')]">
-            <p className="text-3xl text-white font-bold p-10">Datasets from Kaggle,<br></br> Game covers scrapped from Google </p>
+        <div className="w-screen lg:w-1/2 h-1/2">
+          <div className="lg:h-screen h-full flex items-end bg-cover bg-[url('/src/assets/lorenzo-herrera-p0j-mE6mGo4-unsplash.jpg')]">
+            <p className="text-2xl lg:text-3xl text-white font-bold p-10">Datasets from Kaggle,<br></br> Game covers scrapped from Google </p>
           </div>
         </div>
+
       </div>
 
       <div className="h-screen flex flex-col" >
         <div className="h-1/2 flex items-center justify-center">
-          <p className="text-7xl font-bold h-1/4">Our Team.</p>
+          <p className="text-7xl md:text-8xl lg:text-9xl font-bold h-1/4">Our Team.</p>
         </div>
 
-        <div className="h-1/2 flex ">
-          <div className="w-1/2 p-10">
-          <p className="text-4xl "><strong>Banatech</strong> is a dedicated software development company which strives for quality and customer satisfaction.</p>
+        <div className="h-1/2 flex flex-col lg:flex-row ">
+          <div className="w-screen lg:w-1/2 p-10">
+          <p className="text-3xl lg:text-4xl "><strong>Banatech</strong> is a dedicated software development company which strives for quality and customer satisfaction.</p>
           </div>
 
-          <div className="w-1/2 p-10">
+          <div className="w-screen lg:w-1/2 p-10">
             <p className="text-2xl font-bold">Developers</p>
             <p className="text-2xl  mt-10">Sirine Aoudj</p>
             <p className="text-2xl  mt-5">Le Duytam Ly</p>
-            <p className="text-2xl  mt-5">Rodrigo Rivas Alfaro</p>
+            <p className="text-2xl  mt-5">Rodrigo Rivas</p>
             <p className="text-2xl  mt-5">Jei Wen Wu</p>
             <p className="text-2xl  mt-5">Asli Zeybek</p>
           </div>
