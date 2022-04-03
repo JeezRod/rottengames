2f9365b859f768a7bd723c37c2f6e8c8af5bb27b
Le Duytam Ly <le.ly@dawsoncollege.qc.ca>
2022-04-03 14:43:22 -0400

    Add translation for about page

--- client/src/languages/en.js
+++ client/src/languages/en.js
@@ -16,7 +16,14 @@ export default {
 
     logout:{
         logout: "Logout"
-    }
+    },
 
-    
+    about:{
+        about: "About.",
+        description: "All Game reviews in one place.",
+        sources: "Datasets from Kaggle, Game covers scrapped from Google ",
+        team: "Our Team.",
+        banatech: "<strong>Banatech</strong> is a dedicated software development company which strives for quality and customer satisfaction.",
+        developers: "Developers"
+    }    
 }
--- client/src/languages/fr.js
+++ client/src/languages/fr.js
@@ -16,5 +16,15 @@ export default {
 
     logout:{
         logout: "Déconnecter"
+    },
+
+
+    about:{
+        about: "A propos.",
+        description: "Toutes les commentaires dans un seul endroit.",
+        sources: "Ensembles de données de Kaggle, Couvertures de jeux de Google",
+        team: "Notre équipe.",
+        banatech: "<strong>Banatech</strong> est une compagnie de développement de logiciels dédiée qui vise la qualité et la satisfaction du client.",
+        developers: "Développeurs"
     }
 }
